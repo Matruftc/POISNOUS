54c3229fe2abf3e2acdef65a3c0179b738ef7391
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-09 21:17:07 +0530

    initial commit

---
+++ divison.py
@@ -0,0 +1,8 @@
+number1=20
+number2=2
+
+remainderOfTwoNumberIs=number1%number2
+divisonOfTwoNumberIs=number1/number2
+
+print("Remainder of two number is:  " , remainderOfTwoNumberIs)
+print("Divison of two number is:  " , divisonOfTwoNumberIs)

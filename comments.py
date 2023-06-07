5d044691f1b3fbe971b7f5223a22bae6f26472af
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-08 00:05:18 +0530

    initial commit

---
+++ comments.py
@@ -0,0 +1,11 @@
+#i am a single line comment
+
+comment='''in this code i am going to show you how many types of comment and how its used '''
+print(comment)
+
+'''
+i am 
+a
+multi line
+comment
+'''

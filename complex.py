b483fffaaeaad119de6127e25f8f16150714014a
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-08 00:04:44 +0530

    initial commit

---
+++ complex.py
@@ -0,0 +1,11 @@
+number=3j   #the imaginary part is complex
+print(number)
+print(type(number))
+
+number1=3j+4  #the imaginary part is complex
+print(number1)
+print(type(number1))
+
+number2=3j-3   #the imaginary part is complex
+print(number2)
+print(type(number2))

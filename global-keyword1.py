68b7bd072b85b020350933e68d86ac71fffe31c4
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-08 00:03:17 +0530

    initial commit

---
+++ global-keyword1.py
@@ -0,0 +1,9 @@
+comment="  i am the villan"
+
+def global_keyword_sikhunga_mai_aaj():
+    global comment
+    comment="  i am the hero"          #here is the update one
+    
+global_keyword_sikhunga_mai_aaj()
+
+print("Hari said"  + comment)    

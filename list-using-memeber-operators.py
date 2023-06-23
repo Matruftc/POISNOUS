566944f330c45a318a386b263c2f9f5409faec7c
Matru Prasad Mishra <matru19042003@gmail.com>
2023-06-24 00:21:00 +0530

    initial commit

---
+++ list-using-memeber-operators.py
@@ -0,0 +1,7 @@
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+  
+  thislist = ["apple", "banana", "cherry"]
+if "kiwi" not in thislist:
+  print("Yes, 'kiwi' was not in the fruits list")

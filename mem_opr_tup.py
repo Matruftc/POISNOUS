518644344584d80223c941a422f0ec57e345b234
Matru Prasad Mishra <matru19042003@gmail.com>
2023-10-11 20:49:15 +0530

    initial commit

---
+++ mem_opr_tup.py
@@ -0,0 +1,11 @@
+fruits=("apple","banana","chery","coconut","litchi","guava")
+if "banana" in fruits:
+    print("Yes is it")
+else:
+    print("No its not ")
+    
+fruits=("apple","banana","chery","coconut","litchi","guava")
+if "avocado" not in fruits:
+    print("not available")
+else:
+    print("available")    

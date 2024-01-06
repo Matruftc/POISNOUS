844b11420557b3116b8eb377e4d4b2955fc566ac
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-06 20:30:07 +0530

    initial commit

---
+++ strftime.py
@@ -0,0 +1,18 @@
+
+import datetime 
+
+x=datetime.datetime(2013,2,21)
+
+print(x.strftime("%B")) #Month Name
+
+print(x.strftime("%A"))  #day
+
+print(x.strftime("%m"))  #Month Number
+
+print(x.strftime("%w"))  #weekday
+
+print(x.strftime("%C"))  #Century
+
+print(x.strftime("%p"))  #AM/PM
+
+print(x.strftime("%Z"))  #TimeZone

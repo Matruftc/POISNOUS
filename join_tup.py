df7de0212e7bbf84a22da30eadbd7a940f1bddc5
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-06 19:54:00 +0530

    initial commit

---
+++ join_tup.py
@@ -0,0 +1,16 @@
+print(".......................................")
+
+fruits=("mango","cherry","banana","litchi")
+fruit =("aampana","dahi","soya")
+
+res=fruits+fruit                           #addition of two tuple
+print(res)
+
+print(".......................................")
+
+fruits=("mango","cherry","banana","litchi")
+
+res=fruits*2                                #values are multiplied 
+print(res)
+
+    

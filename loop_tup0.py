b3fb2a7e0d3b467a23a7abef7e3a02e2a95afe27
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-06 19:53:38 +0530

    initial commit

---
+++ loop_tup0.py
@@ -0,0 +1,24 @@
+print(".......................................")
+
+fruits=("mango","cherry","banana","litchi")
+
+i=0
+
+while(i<=len(fruits)):
+    
+        print(i)
+        i=i+1
+ 
+ 
+print(".......................................")
+
+fruits=("mango","cherry","banana","litchi")
+
+i=0
+
+while(i<len(fruits)):
+    
+        print(fruits[i])
+        i=i+1
+            
+    

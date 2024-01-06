ad86c5fdabcc991d9715b545f3bad452c0453d6f
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-06 19:46:51 +0530

    initial commit

---
+++ statistics_median.py
@@ -0,0 +1,10 @@
+import statistics
+
+marks=[12,20,17,19,17]
+
+print("This is the median of marks: ",statistics.median(marks))
+
+print("This is the median of marks: ",statistics.median_high(marks))
+
+print("This is the median of marks: ",statistics.median_low(marks))
+

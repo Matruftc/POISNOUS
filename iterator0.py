a54af9a6e51d4d3a6c0386cf6cf4ffcca75e6d81
Matru Prasad Mishra <matru19042003@gmail.com>
2024-02-02 13:58:32 +0530

    initial commit

---
+++ iterator0.py
@@ -0,0 +1,9 @@
+fruits=("banana","cherry","apple","tomato",)
+print(fruits)
+
+iterator_wala = iter(fruits)
+
+print(next(iterator_wala))
+print(next(iterator_wala))
+print(next(iterator_wala))
+print(next(iterator_wala))
